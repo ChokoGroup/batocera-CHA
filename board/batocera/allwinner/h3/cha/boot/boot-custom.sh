3365be26a017bc0784815ee636fbbf7553be8c85
cjom <55603581+cjom@users.noreply.github.com>
2024-04-10 22:09:33 +0100

    Fix renaming partitions on first boot

--- board/batocera/allwinner/h3/cha/boot/boot-custom.sh
+++ board/batocera/allwinner/h3/cha/boot/boot-custom.sh
@@ -2,42 +2,53 @@
 
 case "$1" in
   start)
-	clear > /dev/tty0
-	mount -o remount,rw /boot
-	if [ -e /boot/del-boot-custom ]
-	then
-	  rm -f /boot/boot-custom.sh /boot/del-boot-custom
-	  sync
-	  mount -o remount,ro /boot
-	else
-	  SHAREPART=$(/usr/bin/batocera-part share_internal)
-	  if [ -n "$SHAREPART" ] && [ -e /dev/mmcblk1boot0 ]
-	  then
-      if [ "$(/usr/bin/batocera-part boot)" = "/dev/mmcblk0p1" ] &&  [ "$SHAREPART" = "/dev/mmcblk0p3" ]
-      then
-        echo "Changing BATOCERA_DISK partition label to BATOCERA_DISK_SD" > /dev/tty0
-        e2label /dev/mmcblk0p3 BATOCERA_DISK_SD || { echo "Something went wrong renaming BATOCERA_DISK partition to BATOCERA_DISK_SD!" > /dev/tty0; sleep 4; poweroff -f; }
-        echo "Changing CHA_DISK partition label to CHA_DISK_SD" > /dev/tty0
-        e2label /dev/mmcblk0p2 CHA_DISK_SD || { echo "Something went wrong renaming CHA_DISK partition to CHA_DISK_SD!" > /dev/tty0; sleep 4; poweroff -f; }
-        echo "Changing boot partition label to CHA_BOOT_SD" > /dev/tty0
-        fatlabel /dev/mmcblk0p1 CHA_BOOT_SD || { echo "Something went wrong renaming CHA_BOOT partition to CHA_BOOT_SD!" > /dev/tty0; sleep 4; poweroff -f; }
-        sed -i 's/CHA_BOOT /CHA_BOOT_SD /' /boot/extlinux/extlinux.*
-        echo "Done with partitions labels! Rebooting..." > /dev/tty0
-      fi
-      touch /boot/del-boot-custom
-      rm -f /boot/extlinux/extlinux.conf
+    clear > /dev/tty0
+    if [ -e /boot/del-boot-custom ]
+    then
+      mount -o remount,rw /boot
+      rm -f /boot/boot-custom.sh /boot/del-boot-custom
       sync
       mount -o remount,ro /boot
-      sleep 2
-      reboot
+    else
+      BOOTPART="$(/usr/bin/batocera-part boot)"
+      SHAREPART="$(/usr/bin/batocera-part share_internal)"
+      if [ -n "$BOOTPART" ] && [ -n "$SHAREPART" ] && [ "${BOOTPART%p?}" = "${SHAREPART%p?}" ]
+      then
+        mount -o remount,rw /boot
+        # If there are two, SD card is present
+        if [ "$(ls -1 /dev/mmcblk? | wc -l)" = "2" ]
+        then
+          echo "Changing BATOCERA_DISK partition label to BATOCERA_DISK_SD" > /dev/tty0
+          e2label "$SHAREPART" BATOCERA_DISK_SD || { echo "Error renaming BATOCERA_DISK partition to BATOCERA_DISK_SD!" > /dev/tty0; sleep 5; poweroff -f; }
+          echo "Changing CHA_DISK partition label to CHA_DISK_SD" > /dev/tty0
+          e2label "${BOOTPART%?}2" CHA_DISK_SD || { echo "Error renaming CHA_DISK partition to CHA_DISK_SD!" > /dev/tty0; sleep 5; poweroff -f; }
+          echo "Changing boot partition label to CHA_BOOT_SD" > /dev/tty0
+          fatlabel "$BOOTPART" CHA_BOOT_SD || { echo "Error renaming CHA_BOOT partition to CHA_BOOT_SD!" > /dev/tty0; sleep 5; poweroff -f; }
+          sed -i 's/CHA_BOOT /CHA_BOOT_SD /' /boot/extlinux/extlinux.*
+        else
+          echo "Changing BATOCERA_DISK partition label to BATOCERA_DISK_MM" > /dev/tty0
+          e2label "$SHAREPART" BATOCERA_DISK_MM || { echo "Error renaming BATOCERA_DISK partition to BATOCERA_DISK_MM!" > /dev/tty0; sleep 5; poweroff -f; }
+          echo "Changing CHA_DISK partition label to CHA_DISK_MM" > /dev/tty0
+          e2label "${BOOTPART%?}2" CHA_DISK_MM || { echo "Error renaming CHA_DISK partition to CHA_DISK_MM!" > /dev/tty0; sleep 5; poweroff -f; }
+          echo "Changing boot partition label to CHA_BOOT_MM" > /dev/tty0
+          fatlabel "$BOOTPART" CHA_BOOT_MM || { echo "Error renaming CHA_BOOT partition to CHA_BOOT_MM!" > /dev/tty0; sleep 5; poweroff -f; }
+          sed -i 's/CHA_BOOT /CHA_BOOT_MM /' /boot/extlinux/extlinux.*
+        fi
+        echo -e "Done renaming partitions labels.\nRebooting..." > /dev/tty0
+        touch /boot/del-boot-custom
+        rm -f /boot/extlinux/extlinux.conf
+        sync
+        mount -o remount,ro /boot
+        sleep 2
+        reboot
       else
-      echo "Something is wrong with SD card or eMMC partitions order!" > /dev/tty0
-      echo "This is how they are listed now:" > /dev/tty0
-      lsblk -o name,mountpoint,label /dev/mmcblk??* > /dev/tty0
-      echo "Remove the power cable and try again after a couple of minutes." > /dev/tty0
-      poweroff
-	  fi
-	fi
+        echo -e "Something is wrong with boot or Batocera partition!\nDEBUG INFO:\nBOOTPART=\"$BOOTPART\"   (/boot)\nSHAREPART=\"$SHAREPART\"   (/userdata)" > /dev/tty0
+        lsblk -o name,mountpoint,label /dev/mmcblk??* > /dev/tty0
+        echo -e "\nRemove the power cable and try again after a couple of minutes." > /dev/tty0
+        sleep 10
+        poweroff
+      fi
+    fi
   ;;
   *)
     exit 0
